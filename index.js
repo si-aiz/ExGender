5722b3e3ee97f6b52e3cd55772f045f401ba3018
si-aiz <122490110+si-aiz@users.noreply.github.com>
2023-01-31 11:21:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
 function badel(){
-    femele= ["AngryFemel.jfif","ConfusedFemel.jpg","HappyFemel.jpg","SadFemel.png"];
+    femele =["AngryFemel.jfif","ConfusedFemel.jpg","HappyFemel.jpg","SadFemel.png"];
     male = ["AngyMale.jpg","ConfusedeMale.jpg",'HappyMale.jpg','SadMale.jpg'];
-    color =["red","yellow","pink","green"];
+    color =["red","purple","yellow","blue"];
+    des =["angry",'confused','happy','sad'];
     b= Math.floor(Math.random() * 4);
     a= document.getElementsByName('aziz');
-    c=document.getElementById('des')
+    c=document.getElementById('des');
     img = document.getElementById('image')
     for(i = 0; i < a.length; i++) {
         if(a[i].checked){
             if(a[i].value=='male'){
-                img.src=male[b]
-                switch(b){
-                    case 0:c.innerHTML='angy';
-                        c.style.backgroundColor=color[b];   break;
-                    case 1:c.innerHTML='consused';
-                         c.style.backgroundColor=color[b];  break;
-                    case 2:c.innerHTML='happy';
-                         c.style.backgroundColor=color[b];  break;
-                    case 3:c.innerHTML='sad';
-                         c.style.backgroundColor=color[b];  break;
-                }
+                img.src=male[b];
+                c.innerHTML=des[b];
+                c.style.backgroundColor=color[b];
             }
-            else if(a[i].value=="femele"){
-                img.src=femele[b]
-                switch(b){
-                    case 0:c.innerHTML='angy';
-                         c.style.backgroundColor=color[b];  break;
-                    case 1:c.innerHTML='consused';
-                         c.style.backgroundColor=color[b];  break;
-                    case 2:c.innerHTML='happy';
-                         c.style.backgroundColor=color[b];  break;
-                    case 3:c.innerHTML='sad';
-                         c.style.backgroundColor=color[b];  break;
-                }
+            else{
+                img.src=femele[b];
+                c.innerHTML=des[b];
+                c.style.backgroundColor=color[b];
             }
         }
     }
     if(c.innerHTML=='what?'){
-        c.innerHTML='select gender'
+        c.innerHTML='select gender'; 
     }
-    
 }
+
+
